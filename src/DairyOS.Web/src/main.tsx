29864fc57af5483141f7c2a8500da16cc052a425
ammad4147 <ammad4147@gmail.com>
2026-08-23 03:35:04 +0500

    fix(ui): activate production cost display bridge

--- src/DairyOS.Web/src/main.tsx
+++ src/DairyOS.Web/src/main.tsx
@@ -2,6 +2,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AlertAuditProvider } from './context/AlertAuditContext';
+import { installUiTerminologyBridge } from './uiTerminologyBridge';
+
+installUiTerminologyBridge();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
