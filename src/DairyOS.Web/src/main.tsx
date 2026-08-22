894088fd17394cafbc3bb36f14af284c3f3221c1
ammad4147 <ammad4147@gmail.com>
2026-08-23 03:48:58 +0500

    fix(dashboard): remove UI terminology runtime bridge

--- src/DairyOS.Web/src/main.tsx
+++ src/DairyOS.Web/src/main.tsx
@@ -2,9 +2,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AlertAuditProvider } from './context/AlertAuditContext';
-import { installUiTerminologyBridge } from './uiTerminologyBridge';
-
-installUiTerminologyBridge();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
