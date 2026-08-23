44f8b6e721d532be7cbe92fe2ee20b54fc209923
ammad4147 <ammad4147@gmail.com>
2026-08-23 12:26:50 +0500

    feat(ui): attach authenticated token to API requests

--- src/DairyOS.Web/src/main.tsx
+++ src/DairyOS.Web/src/main.tsx
@@ -1,7 +1,10 @@
-﻿import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AlertAuditProvider } from './context/AlertAuditContext';
+import { installAuthenticatedFetch } from './auth';
+
+installAuthenticatedFetch();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
