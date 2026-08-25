0e5960cd1dddd3d8df8455763f015689fef6bb29
ammad4147 <ammad4147@gmail.com>
2026-08-25 20:33:17 +0500

    Register user and email models with ORM metadata

--- src/dairyos/data/models/__init__.py
+++ src/dairyos/data/models/__init__.py
@@ -9,6 +9,11 @@ from .feed_ration import FeedRation
 from .feed_inventory_item import FeedInventoryItem
 from .health_observation import HealthObservation
 from .coml_record import COMLRecord
+from .user import User
+from .app_setting import AppSetting
+from .email_sender_setting import EmailSenderSetting
+from .email_digest_run import EmailDigestRun
+from .email_digest_delivery import EmailDigestDelivery
 
 
 __all__ = [
@@ -23,4 +28,9 @@ __all__ = [
     "FeedInventoryItem",
     "HealthObservation",
     "COMLRecord",
+    "User",
+    "AppSetting",
+    "EmailSenderSetting",
+    "EmailDigestRun",
+    "EmailDigestDelivery",
 ]
